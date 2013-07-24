68786b0dbd5262f1b036b497b4e0aaaa41772c11
Luke Kavanagh <ltkavanagh@gmail.com>
2013-07-24 11:24:51 +0100

    Improve documenting comments on spreadsheet config.js

--- app/templates/config.js
+++ app/templates/config.js
@@ -3,16 +3,55 @@
 var app = app || {};
 
 app.spreadsheet = {
-
+<% if (!spreadsheetId) { %>
+  // TODO: insert Bertha Spreadsheet ID here
+<% } %>
   id: '<%= spreadsheetId %>',
 
   // Optional Bertha settings
   options: {
-    preset: '', // 'profiles', 'timelines'
-    method: '', // 'long', 'short' or 'view'
-    // host: '', // to point to a different Bertha server
-    // plugin: '', // 'ig' or 'gss',
-    // sheets: [], // an array of custom sheets
+
+    /**
+    * Preset
+    *
+    * Only use in combination with the 'ig' plugin
+    *
+    * Available presets
+    *    - profiles
+    *    - timelines
+    */
+    //preset: '',
+
+    /**
+    * Method
+    *
+    * Available methods:
+    *   - long (default): long cache headers
+    *   - short: short cache headers
+    *   - view: no caching - most basic method
+    */
+    method: 'view'//,
+
+    /**
+    * Host
+    *
+    * To override the Bertha server that the
+    * data is coming from
+    */
+    // host: '',
+
+    /**
+    * Which Bertha plugin to use
+    * Available plugins:
+    *   - ig (default)
+    *   - gss
+    */
+    // plugin: '',
+
+    /*
+    * An array of custom sheets
+    */
+    // sheets: []
   }
 
 };
