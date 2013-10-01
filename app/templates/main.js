7291bb631926cbc3b8a44fad23727338c92fe73b
Callum Locke <callum.locke@gmail.com>
2013-10-01 13:50:19 +0100

    fix jshint error

--- app/templates/main.js
+++ app/templates/main.js
@@ -24,7 +24,9 @@
 
     // REMOVE: these two lines once you are happy that the templates are working as expected
     console.log('You can see what template helpers have been registered here:', Handlebars.helpers);
-    window.JST && console.log('Templates have been attached to JST', JST);
+    if (window.JST) {
+        console.log('Templates have been attached to JST', JST);
+    }
 <% } %>
 
 
